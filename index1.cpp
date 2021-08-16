e3dbb36ddf43c12ada57e8937d5067986f06d760
varath-ta <varath.surya@teamta.in>
2021-08-16 17:27:12 +0530

    name

--- index1.cpp
+++ index1.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
-class merchant{
-  
+class merchant
+{
+
 }
 
-int main() {
-  cout << "Hello World!";
+int
+main()
+{
+  cout << "E-commerce system";
   return 0;
 }
