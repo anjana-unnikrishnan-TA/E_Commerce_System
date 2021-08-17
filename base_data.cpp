046dd7566c967486086e8f0cc5832242bbc0f083
anjana-unnikrishnan-TA <anjana.unnikrishnan@teamta.in>
2021-08-17 10:56:38 +0530

    Class Declaration for E Commerce System

---
+++ base_data.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+class User_Register_and_Login
+{
+    private:
+        char user_Name[20];
+        double contact_Num;
+        char email[30];
+        int user_Type;
+        int user_Id;
+    public:
+        void get_User_Data();
+        void get_Name();
+        void get_Usertype();
+        void set_UserId();
+        void registeration();
+        void login();
+};
+
+class Product
+{
+    private:
+        char product_Name[20];
+        char prod_Type[20];
+        double prod_Price;
+        int count;
+    public:
+        void get_Product_Data();
+        void put_Product_Data();
+        void get_Product_Name();
+        void get_Product_Type();
+        void get_Product_Price();
+        void get_Product_Count();
+};
+
+class Order
+{
+    private:
+        char order_Status[20];
+    public:
+        void display_Order_Status();
+        void order_Cancellation();
+        void order_Placement();
+        void cancelled_Orders_List();     
+        void pending_Orders_List();
+        void completed_Orders_List();
+};
+
+class Merchant
+{
+    public:
+        void add_Product();
+        void search_Product();
+        void list_Product_Count();
+        void assign_Courier_Person();
+        //inherit cancelled_Orders_List(); and display_Order_Status(); from order class
+
+};
+
+class Customer
+{
+    public:
+        void customer_Search_Product();
+        void buy_Product();
+        void cancel_Order();
+        //inherit display_Order_Status(); from order class
+};
+
+class Courier_Person
+{
+    public:
+        void update_Order_Status();
+        //inherit assign_Courier_Person(); from merchant class and, pending_Orders_List(); , completed_Orders_List(); from order class
+};
